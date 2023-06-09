7a5fa7a51e8c7e01ad496a04561f76968d139374
bayun2445 <52805985+bayun2445@users.noreply.github.com>
2023-06-09 20:52:44 +0700

    Added home UI layout

--- app/src/main/java/com/linguity/app/ui/main/MainActivity.kt
+++ app/src/main/java/com/linguity/app/ui/main/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.linguity.app.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.linguity.app.R
+import com.linguity.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
